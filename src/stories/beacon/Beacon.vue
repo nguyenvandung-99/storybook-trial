fd77e251ba46aad996ad89704ccc1aa2749a2f65
Daniel Nguyen <daniel.nguyen@print.com>
2022-05-21 12:29:54 +0200

    update effect

--- src/stories/beacon/Beacon.vue
+++ src/stories/beacon/Beacon.vue
@@ -127,6 +127,13 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+
 .beacon {
   &-wrapper {
     position: relative;
